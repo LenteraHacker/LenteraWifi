1626453eee04c550d33277dfcadf9573166a5652
DW SQUAD OFFICAL <54875746+LenteraHacker@users.noreply.github.com>
2019-09-21 14:08:34 +0800

    hack.sh

---
+++ hack.sh
@@ -0,0 +1,42 @@
+clear
+echo "DW SQUAD WI-FI HACKER"
+echo "DW SQUAD WI-FI HACKER"
+echo "  /|       |\"
+echo "/  |       |  \"
+echo "|    \     /    |"
+echo "|  \ /     \ /  |"
+echo "| \  |     |  / |"
+echo "| \ _\_/^\_/_ / |"
+echo "|    --\//--    |"
+echo " \_  \     /  _/"
+echo "   \__  |  __/"
+echo "      \ _ /"
+echo "      /   \"
+echo "    / _/|\_ \ DW SQUAD"
+sleep 5
+echo "Searching Your Location...."
+sleep 10
+echo "Found!! you in SMP Lentera Harapan School"
+sleep 3
+echo "Getting vuln WI-FI...."
+sleep 15
+echo "The wifi password found!! showing..."
+sleep 3
+echo "Office SLH : blessslh"
+sleep 5
+echo "SMA Lentera : SMALentera"
+sleep 6
+echo "Lentera Community : trulyyours"
+sleep 7
+echo "SLH Kupang : 4lf93g9o"
+sleep 2
+echo "Creators : DW SQUAD"
+echo "The members (DEWA, ABI ,RICHARD[LEADER], ALEGRA, BERHAN, ENJI, TIKA, DAVIS, HIZKYA, GYAN, NADINE, JESSTRY, VIKI, AND 2 MEMBER MORE"
+sleep 2
+echo "maybe my face is ugly, but dont ask about this.."
+sleep 2
+echo "exit...."
+exit
+esac
+done
+done
